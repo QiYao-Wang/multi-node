ef24b1ac93c17e1d2d6cb6b8a0bae99a54003f11
Mathsion <2646769409@qq.com>
2025-06-20 19:19:29 +0800

    Update manual_script.sh

--- init_debug/manual_script.sh
+++ init_debug/manual_script.sh
@@ -1,1 +1,20 @@
+module load brics/singularity-multi-node
 
+export NNODES=2
+export MASTER_ADDR=your-master-noode-id
+
+srun --overlap --jobid your-master-node-jobid --nodelist=your-master-noode-id --mpi=pmix singularity exec --nv -H your-env-path.sif /bin/bash -c "export TRITON_LIBCUDA_PATH=/usr/local/cuda/compat/lib.real; \
+cd your-project-path; \
+echo `pwd`; \
+export MASTER_ADDR=your-master-noode-id; \
+export NNODES=2; \
+export NODE_RANK=0; \
+/bin/bash init_debug.sh"
+
+srun --overlap --jobid your-second-node-jobid --nodelist=your-second-noode-id --mpi=pmix singularity exec --nv -H your-env-path.sif /bin/bash -c "export TRITON_LIBCUDA_PATH=/usr/local/cuda/compat/lib.real; \
+cd your-project-path; \
+echo `pwd`; \
+export MASTER_ADDR=your-master-noode-id; \
+export NNODES=2; \
+export NODE_RANK=1; \
+/bin/bash init_debug.sh"
